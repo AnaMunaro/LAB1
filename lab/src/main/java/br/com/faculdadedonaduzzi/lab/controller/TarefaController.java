dd0689ec483e3305e8603ef277415e9d67acbea0
AnaMunaro <munaroapavaoa@gmail.com>
2026-05-26 21:00:34 -0300

    fix: corrige nomenclatura da rota no teste unitario

--- lab/src/main/java/br/com/faculdadedonaduzzi/lab/controller/TarefaController.java
+++ lab/src/main/java/br/com/faculdadedonaduzzi/lab/controller/TarefaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tarefas")
+@RequestMapping("/tarefa")
 public class TarefaController {
 
     @Autowired
